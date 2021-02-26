a6fea1c1baf48fcda0e28c530d68d34245e0d2f4
Richard Moore <richard.h.moore.jr@gmail.com>
2021-02-26 10:47:16 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci_Iterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Fib_Iter(4);
+        }
+
+        public static void Fib_Iter(int len)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write($"{a} {b}");
+
+            for(int x=0; x<len; x++)
+            {
+                c = a + b;
+                Console.Write($" {c}");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
